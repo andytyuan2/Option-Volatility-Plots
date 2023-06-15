249e923d471a1acdfe565a950cf52bd406f93c5b
Andy Yuan <andy.yuan2tdsb@gmail.com>
2023-06-15 15:09:38 -0400

    added color

--- Volatility Smile.py
+++ Volatility Smile.py
@@ -2,6 +2,9 @@ from datetime import date
 from datetime import datetime as dt
 import yfinance as yf
 import matplotlib.pyplot as plt
+from matplotlib import cm
+# X axis = time to maturity, Y axis = strike, Z axis = implied volatility
+dict = {}
 
 # TICKER
 ticker = 'aapl'                                             # not case sensitive
@@ -9,10 +12,13 @@ stock = yf.Ticker(ticker)
 
 # CALL VS PUT
 callput = 1                                                 # call = 1, put = -1; in callput
+if callput == 1:
+    optionname = 'Calls'
+elif callput == -1:
+    optionname = 'Puts'
 
 # EXPIRATION LIST
 expiration = list(stock.options)
-print('expiration dates are', expiration)
 
 # EXPIRY DATE
 date_of_exp = expiration[0]
@@ -41,9 +47,8 @@ vol = list(option_info[date_of_exp]['impliedVolatility'])
 price = stock.info['currentPrice']
 
 
-plt.scatter(strikes, vol)
-plt.axvline(price)
+plt.scatter(strikes, vol, c=vol, cmap = cm.jet)
 plt.ylabel('Implied Volatility (%)')
 plt.xlabel('Strikes($)')
-plt.title('Volatility smile for $'+ticker+': Implied Volatility as a Function of Strike')
+plt.title('Volatility Smile for $'+ticker+' '+optionname+': Implied Volatility as a Function of Strike')
 plt.show()
